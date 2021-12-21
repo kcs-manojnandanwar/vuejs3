c4feba5bf36f33bf4dd246f75067c65d5db3f4c5
Manoj Nandanwar <manoj.nandanwar@kcsitglobal.com>
2021-12-21 14:19:04 +0530

    events listener

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-VTTCue.createApp({
+const app = Vue.createApp({
     data(){
         return{
-            goals:[],
-            enteredValue:''
-
-        };
+           counter : 0,
+           name: ''
 
 
+           };
+        
     },
     methods:{
-        addGoal(){
-            this.goals.push(this.enteredValue);
+        setname(event, lastname){
+           this.name= event.target.value + ' ' + lastname;
+        },
+        add(num){
+            this.counter= this.counter+num;
+           
+            },
+           reduce(num){
+               this.counter = this.counter-num;
 
+           
+            }
         }
 
 
-    }
+    });
 
 
-}).mount('#app');
+
+app.mount('#event');
